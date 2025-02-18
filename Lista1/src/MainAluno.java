7982dc4a31b88377c2af62b4424503fa516574da
Daniel <dfpires@gmail.com>
2025-02-18 10:23:56 -0300

    classe Aluno

--- Lista1/src/MainAluno.java
+++ Lista1/src/MainAluno.java
@@ -1,4 +1,16 @@
 import exe1.Aluno;
-public class MainAluno {
 
+import javax.swing.*;
+
+public class MainAluno {
+    public static void main(String[] args) {
+        Aluno obj1 = new Aluno();
+        obj1.idade = Integer.parseInt(JOptionPane.showInputDialog("Idade"));
+        obj1.nroAluno = Integer.parseInt(JOptionPane.showInputDialog("Nro Aluno"));
+        obj1.p1 = Float.parseFloat(JOptionPane.showInputDialog("P1"));
+        obj1.p2 = Float.parseFloat(JOptionPane.showInputDialog("P2"));
+        obj1.nome = JOptionPane.showInputDialog("Nome");
+
+        Aluno obj2 = new Aluno(19, 123, 6, 8.5f, "Fulano");
+    }
 }
--- Lista1/src/exe1/Aluno.java
+++ Lista1/src/exe1/Aluno.java
@@ -1,4 +1,18 @@
 package exe1;
 
 public class Aluno {
+    public String nome;
+    public int nroAluno, idade;
+    public float p1, p2;
+    // construtor sem parâmetros
+    public Aluno(){
+        this.nome = "nome não definido";
+    }
+    public Aluno(int idade, int nroAluno, float p1, float p2, String nome){
+        this.idade = idade;
+        this.nroAluno = nroAluno;
+        this.p1 = p1;
+        this.p2 = p2;
+        this.nome = nome;
+    }
 }
