ef7213c9755d6de722d2e9111896efbb9dde96f1
Daniel <dfpires@gmail.com>
2025-04-22 10:45:45 -0300

    correção prova

--- Lista4/src/prova/Instalacao.java
+++ Lista4/src/prova/Instalacao.java
@@ -1,4 +1,34 @@
 package prova;
 
 public class Instalacao extends ObraArte{
+    private boolean interativa;
+    public Instalacao(){
+        super();
+    }
+    public Instalacao(boolean interativa, String titulo, String artista){
+        super(titulo, artista);
+        this.interativa = interativa;
+    }
+
+    public boolean isInterativa() {
+        return interativa;
+    }
+
+    public void setInterativa(boolean interativa) {
+        this.interativa = interativa;
+    }
+
+    @Override
+    public String toString() {
+        return "Instalacao{" +
+                super.toString() +
+                "interativa=" + interativa +
+                '}';
+    }
+
+    @Override
+    public String descrever() {
+        return this.interativa ?
+                "Instalação Interativa": "Instalação não interativa";
+    }
 }
