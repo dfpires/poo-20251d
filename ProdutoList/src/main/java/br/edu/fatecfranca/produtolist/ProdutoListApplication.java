3d3b1e3a482344436b8fbe1a6c160ecfd46e51f8
Daniel <dfpires@gmail.com>
2025-04-28 09:39:27 -0300

    Lista de Produtos

---
+++ ProdutoList/src/main/java/br/edu/fatecfranca/produtolist/ProdutoListApplication.java
@@ -0,0 +1,14 @@
+package br.edu.fatecfranca.produtolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoListApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProdutoListApplication.class, args);
+    }
+
+}
