64910d5f5a1062ef96aa27c4083cb6664c6267f6
Daniel <dfpires@gmail.com>
2025-05-19 08:46:37 -0300

    Produto BD finalizado

--- ProdutoBD/src/main/java/br/edu/fatecfranca/produtobd/controller/ProdutoController.java
+++ ProdutoBD/src/main/java/br/edu/fatecfranca/produtobd/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import br.edu.fatecfranca.produtobd.service.ProdutoService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produto")
@@ -31,5 +32,10 @@ public class ProdutoController { // não existe regra de negócio aqui
     public String remover(@PathVariable Long id) {
         return service.removerProduto(id) ?
                 "Remoção com sucesso": "Produto não encontrado";
+    }
+
+    @PutMapping("/{id}")
+    public Optional<Produto> atualizar(@PathVariable Long id, @RequestBody Produto novo) {
+        return service.atualizaProduto(id, novo);
     }
 }
