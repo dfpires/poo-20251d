59b995422ea51e20ffa01613e2f917c0afcb3729
Daniel <dfpires@gmail.com>
2025-06-03 11:00:33 -0300

    escola

--- escola/src/main/java/br/edu/fatecfranca/escola/controller/AlunoController.java
+++ escola/src/main/java/br/edu/fatecfranca/escola/controller/AlunoController.java
@@ -1,4 +1,26 @@
 package br.edu.fatecfranca.escola.controller;
 
+import br.edu.fatecfranca.escola.model.Aluno;
+import br.edu.fatecfranca.escola.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/aluno")
 public class AlunoController {
+
+    private AlunoService alunoService;
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public List<Aluno> getAlunos(){
+        return alunoService.getAlunos();
+    }
+    @PostMapping
+    public Aluno createAluno(@RequestBody Aluno aluno){
+        return alunoService.addAluno(aluno);
+    }
 }
--- escola/src/main/java/br/edu/fatecfranca/escola/service/AlunoService.java
+++ escola/src/main/java/br/edu/fatecfranca/escola/service/AlunoService.java
@@ -1,8 +1,37 @@
 package br.edu.fatecfranca.escola.service;
 
+import br.edu.fatecfranca.escola.model.Aluno;
+import br.edu.fatecfranca.escola.model.Curso;
+import br.edu.fatecfranca.escola.repository.AlunoRepository;
+import br.edu.fatecfranca.escola.repository.CursoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AlunoService {
+    private AlunoRepository alunoRepository;
+    private CursoRepository cursoRepository;
 
+    public AlunoService(AlunoRepository alunoRepository, CursoRepository cursoRepository) {
+        this.alunoRepository = alunoRepository;
+        this.cursoRepository = cursoRepository;
+    }
+
+    // recupera os alunos
+    public List<Aluno> getAlunos(){
+        return alunoRepository.findAll();
+    }
+
+    // adiciona um aluno
+    public Aluno addAluno(Aluno aluno){
+        // verifica se o curso do aluno existe
+        Curso curso =
+        cursoRepository.findById(aluno.getCurso().getId()).orElse(null);
+        if (curso != null){
+            aluno.setCurso(curso); // define o curso completo, e não somente o id
+            return alunoRepository.save(aluno);
+        }
+        return null;
+    }
 }
